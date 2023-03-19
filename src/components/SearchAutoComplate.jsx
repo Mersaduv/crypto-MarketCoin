39a5de96eb591920fe502603a6912c91e7a3a4a1
mersad karimi <mersadkhaha70@gmail.com>
2023-03-19 12:52:31 -0700

    fix bug SearchAutoComplate

--- src/components/SearchAutoComplate.jsx
+++ src/components/SearchAutoComplate.jsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import useAutocomplete from "@mui/base/useAutocomplete";
+
 import { styled } from "@mui/system";
 import { getData } from "../services/serviceData";
 import Link from "next/link";
@@ -7,6 +7,7 @@ import { useRouter } from "next/router";
 import TrendBeta from "./TrendBeta";
 import { FcFlashOn } from "react-icons/fc";
 import StarIcon from "@mui/icons-material/Star";
+import useAutocomplete from "@mui/base/useAutocomplete";
 
 import { Typography } from "@mui/material";
 const Input = styled("input")(({ theme }) => ({
@@ -39,78 +40,75 @@ const Listbox = styled("ul")(({ theme }) => ({
 
 export default function SearchAutoComplate() {
   const [dataCoin, setDataCoin] = React.useState([]);
-
-  const [, setInputValue] = React.useState("");
+  const [highlightedIndex, setHighlightedIndex] = React.useState(0); // initialize with a default value
+  const [isOpen, setIsOpen] = React.useState(false); // add state for list visibility
+  const [selectedOptions, setSelectedOption] = React.useState(null);
   const router = useRouter();
   React.useEffect(() => {
     const fetchDataSearch = async () => {
       try {
         const { data } = await getData();
         setDataCoin(data);
-        console.log(data);
       } catch (error) {
         console.log(error);
       }
     };
     fetchDataSearch();
   }, []);
-
   const {
     getRootProps,
     getInputProps,
     getListboxProps,
     getOptionProps,
     groupedOptions,
-    highlightedIndex,
+    focused,
+    setAnchorEl,
   } = useAutocomplete({
     id: "use-autocomplete-demo",
     options: dataCoin || [],
-    getOptionLabel: (option) => option.name || "",
+    getOptionLabel: (option) => option.name || "" || selectedOptions.name,
   });
-  // Get the value of the first option if it exists
-  const { value, ...inputProps } = getInputProps();
-
-  // Pass the defaultValue prop to the input element using spread syntax.
-  // const handleKeyDown = (event) => {
-  //   if (event.key === "Enter") {
-  //     // find the first option
-  //     const firstOption = groupedOptions[0];
-  //     if (firstOption) {
-  //       // set the input value to the first option's label
-
-  //       // setInputValue(firstOption.name);
-  //       // navigate to the first option's URL
-
-  //       router.push(
-  //         "/cryptoMarket/[coinId]",
-  //         `/cryptoMarket/${firstOption.id}`
-  //       );
-
-  //       console.log(firstOption, event.target.value);
-  //       event.target.value = firstOption.name;
-  //     }
-  //   }
-  // };
+  // if (selectedOptions) {
+  //   console.log(selectedOptions);
+  // }
+  const { onBlur, onFocus, ...inputProps } = getInputProps();
 
   const handleKeyDown = (event) => {
     if (event.key === "Enter") {
-      console.log(highlightedIndex);
-      // find the highlighted option
-      const highlightedOption = groupedOptions[highlightedIndex];
-      if (highlightedOption) {
-        // navigate to the highlighted option's URL
+      // get the highlighted option, or the first option if none are highlighted
+      const selectedOption =
+        groupedOptions[highlightedIndex] || groupedOptions[0];
+
+      // navigate to the selected option's URL
+      if (selectedOption) {
         router.push(
           "/cryptoMarket/[coinId]",
-          `/cryptoMarket/${highlightedOption.id}`
+          `/cryptoMarket/${selectedOption.id}`
         );
-
-        console.log(highlightedOption, event.target.value);
-        event.target.value = highlightedOption.name;
+        setIsOpen(false);
       }
-    }
-    // console.log(value);
-  };
 
+      // update the input value to match the selected option's name
+      if (selectedOption) {
+        event.target.value = selectedOption.name;
+      }
+
+      console.log(highlightedIndex, groupedOptions[highlightedIndex]);
+      // reset highlightedIndex
+      setSelectedOption(selectedOption);
+      setHighlightedIndex(0);
+    } else if (event.key === "ArrowUp") {
+      event.preventDefault();
+      setHighlightedIndex((prevIndex) =>
+        prevIndex === 0 ? groupedOptions.length - 1 : prevIndex - 1
+      );
+    } else if (event.key === "ArrowDown") {
+      event.preventDefault();
+      setHighlightedIndex((prevIndex) =>
+        prevIndex === groupedOptions.length - 1 ? 0 : prevIndex + 1
+      );
+    }
+  };
   return (
     <div className="autoComplate flex items-center gap-x-2">
       <Link href="/watchlist">
@@ -137,11 +135,22 @@ export default function SearchAutoComplate() {
         <Input
           className="px-2 pb-2 pt-1"
           placeholder="جستجو..."
-          onKeyDown={handleKeyDown} // handle keydown event
+          onBlur={(event) => {
+            onBlur(event);
+            if (!focused) {
+              setAnchorEl(null);
+            }
+          }}
+          onFocus={(event) => {
+            onFocus(event);
+            setAnchorEl(document.getElementById("trend"));
+            setIsOpen(true); // open the list when the input is focused
+          }}
+          onKeyDown={handleKeyDown}
           {...inputProps}
         />
       </div>
-      {groupedOptions.length > 0 && (
+      {isOpen && groupedOptions.length > 0 ? (
         <Listbox
           className="flex absolute shadow border-none flex-col w-80 top-10 left-0 "
           {...getListboxProps()}
@@ -158,9 +167,27 @@ export default function SearchAutoComplate() {
               key={option.id}
               {...getOptionProps({
                 option,
-                index,
+                index: option.id,
               })}
-              className="flex hover:bg-gray-100 cursor-pointer p-2 mr-1 justify-between"
+              className={
+                highlightedIndex === index
+                  ? "flex hover:bg-gray-100 cursor-pointer p-2 mr-1 justify-between bg-gray-100"
+                  : "flex hover:bg-gray-100 cursor-pointer p-2 mr-1 justify-between"
+              }
+              onMouseMove={() => {
+                setHighlightedIndex(index);
+              }}
+              onClick={() => {
+                // Same logic as handleKeyDown function when "Enter" key is pressed
+                router.push(
+                  "/cryptoMarket/[coinId]",
+                  `/cryptoMarket/${option.id}`
+                );
+                event.target.value = option.name;
+                setSelectedOption(option);
+                setIsOpen(false); // close the list when an item is clicked
+                setHighlightedIndex(0);
+              }}
             >
               <div className="flex flex-row gap-x-2">
                 <img
@@ -177,7 +204,7 @@ export default function SearchAutoComplate() {
             </li>
           ))}
         </Listbox>
-      )}
+      ) : null}
     </div>
   );
 }
